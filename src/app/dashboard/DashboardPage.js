1e9c8c77fbfd99def4e7703e27cc043576a7b40e
wind <tuanphongmta0205@gmail.com>
2019-09-04 00:39:45 +0700

    up

--- src/app/dashboard/DashboardPage.js
+++ src/app/dashboard/DashboardPage.js
@@ -1,6 +1,21 @@
 import React, {Component} from 'react'
+import {getListCapture} from '../../services/apis/CaptureAPI'
 
 class DashboardPage extends Component{
+
+    componentDidMount(){
+        this._getCapture()
+    }
+
+    _getCapture = async () => {
+        try{
+            console.log(await getListCapture())
+        }
+        catch(e){
+
+        }
+    }
+
     render(){
         return(
             <div className='DashboardPage'>
