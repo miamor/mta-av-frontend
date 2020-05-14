dd03ac0c0085729cb515e7372c864428263908fb
Tu Nguyen <miamorwest@gmail.com>
2020-05-14 07:08:23 +0700

    14.05.20

--- src/app/home/HomePage.js
+++ src/app/home/HomePage.js
@@ -1,14 +1,35 @@
 import React, {Component} from 'react'
 import {Table} from 'reactstrap'
 
-import {table_event} from './sample'
+// import {table_event} from './sample'
 
 class HomePage extends Component{
 
     state = {
         selected: 0,
+        list_capture: [],
+        isLoading: false,
     }
 
+    componentDidMount() {
+        this.setState({ isLoading: true });
+     
+        fetch('http://192.168.1.118:5002/api/v1/capture')
+        .then(response => response.json())
+        .then(data => {
+            data.map((item, index) => {
+                if (item.detected_by == '' || item.detected_by == null) {
+                    item.status = 'OK'
+                } else {
+                    item.status = 'DANGER'
+                }
+            })
+            this.setState({
+                list_capture: data, 
+                isLoading: false 
+            })
+        });
+    }
 
     _getColor = (status) => {
         if (status === 'OK') return 'green'
@@ -20,9 +41,17 @@ class HomePage extends Component{
         this.setState({selected: index})
     }
 
-    render(){
+    render() {
 
-        const {selected} = this.state
+        const { selected, list_capture, isLoading } = this.state
+ 
+        if (isLoading) {
+          return <p>Loading ...</p>
+        }
+
+        if (list_capture.length === 0) {
+            return <p>Empty</p>
+        }
 
         return(
             <div className='HomePage row'>
@@ -39,13 +68,13 @@ class HomePage extends Component{
                         </thead>
                         <tbody>
                         {
-                            table_event.map((item, index) => (
+                            list_capture.map((item, index) => (
                                 <tr key={`domain-${index}`} onClick={() => this._handleSelect(index)} style={{cursor: 'pointer'}}>
                                     <th scope='row'>{index+1}</th>
-                                    <td>{item.name}</td>
-                                    <td>{item.size}</td>
+                                    <td class='file_name'>{item.file_name}</td>
+                                    <td>{item.file_size}</td>
                                     <td>{item.time_received}</td>
-                                    <th style={{color: this._getColor(item.status)}}>{item.status}</th>
+                                    <th style={{color:this._getColor(item.status)}}>{item.status}</th>
                                 </tr>
                             ))
                         }
@@ -53,32 +82,28 @@ class HomePage extends Component{
                     </Table>
                 </div>
                 <div className='Detail col'>
-                    <div className='Header'>{table_event[selected].name}</div>
+                    <div className='Header'>{list_capture[selected].file_name}</div>
                     <Table striped>
                         <tbody>
                             <tr>
                                 <th>Size</th>
-                                <td>{table_event[selected].size}</td>
+                                <td>{list_capture[selected].file_size}</td>
                             </tr>
                             <tr>
                                 <th>Time Received</th>
-                                <td>{table_event[selected].time_received}</td>
+                                <td>{list_capture[selected].time_received}</td>
                             </tr>
                             <tr>
                                 <th>Status</th>
-                                <th style={{color: this._getColor(table_event[selected].status)}}>{table_event[selected].status}</th>
+                                <th style={{color:this._getColor(list_capture[selected].status)}}>{list_capture[selected].status}</th>
                             </tr>
                             <tr>
                                 <th>MD5</th>
-                                <td>{table_event[selected].MD5}</td>
-                            </tr>
-                            <tr>
-                                <th>SHA-1</th>
-                                <td>{table_event[selected].SHA}</td>
+                                <td>{list_capture[selected].md5}</td>
                             </tr>
                             <tr>
                                 <th>File Type</th>
-                                <td>{table_event[selected].type}</td>
+                                <td>{list_capture[selected].file_extension}</td>
                             </tr>
                         </tbody>
                     </Table>     
